8ed5ab7ac2086b6f938d0a907b0bc6c60465d357
yeonmitc <82494535+yeonmitc@users.noreply.github.com>
2023-03-10 08:02:07 +0900

    Initial commit

---
+++ src/main/java/com/example/etst/EtstApplication.java
@@ -0,0 +1,13 @@
+package com.example.etst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/etst/EtstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
